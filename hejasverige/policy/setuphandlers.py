5294e1d3a3262f5604b4005cbf8b28fe2b2eea99
Daniel Grindelid <daniel.grindelid@gmail.com>
2013-01-11 16:50:09 +0100

    Added setup for SiteSecurity
    Init web with possibility to register new accounts, not setting
    password, no user folder and user email as login

--- hejasverige/policy/setuphandlers.py
+++ hejasverige/policy/setuphandlers.py
@@ -1,16 +1,32 @@
+import logging
 from Products.CMFCore.utils import getToolByName
+from plone.app.controlpanel.security import ISecuritySchema
 
-def setupGroups(portal):
-  acl_users = getToolByName(portal, 'acl_users')
-  if not acl_users.searchGroups(name='Staff'):
-    gtool = getToolByName(portal, 'portal_groups')
-    gtool.addGroup('Staff', roles=['StaffMember'])
+def setupSiteSecurity(portal, logger):
+    """
+        site security setup!
+    """
+    secSchema = ISecuritySchema(portal)
+    secSchema.set_enable_self_reg(True)
+    secSchema.set_enable_user_pwd_choice(False)
+    secSchema.set_enable_user_folders(False)
+    secSchema.set_use_email_as_login(True)
+
+def setupGroups(portal, logger):
+    acl_users = getToolByName(portal, 'acl_users')
+    if not acl_users.searchGroups(name='Staff'):
+        gtool = getToolByName(portal, 'portal_groups')
+        gtool.addGroup('Staff', roles=['StaffMember'])
 
 def importVarious(context):
-  """Miscellanous steps import handle
-  """
-  if context.readDataFile('hejasverige.policy-various.txt') is None:
-    return
+    """Miscellanous steps import handle
+    """
+    if context.readDataFile('hejasverige.policy-various.txt') is None:
+        return
+
+    logger = logging.getLogger('hejasverige.policy')
+
+    portal = context.getSite()
+    setupGroups(portal, logger)
+    setupSiteSecurity(portal, logger)
   
-  portal = context.getSite()
-  setupGroups(portal)
